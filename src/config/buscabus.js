9e0dda3c81db1f12a142bf1605312b7eeb76ad7b
alvarollmenezes <alvarollmenezes@gmail.com>
2017-04-26 12:56:36 -0300

    Adiciona cache no proxy do buscabus

--- src/config/buscabus.js
+++ src/config/buscabus.js
@@ -1,5 +1,5 @@
 module.exports = {
-    api: process.env.BUSCABUS_API,
-    user: process.env.BUSCABUS_USER,
-    password: process.env.BUSCABUS_PASSWORD
+    api: process.env.BUSCABUS_API || 'https://buscabus-prodest.geocontrol.com.br',
+    user: process.env.BUSCABUS_USER || 'prodest',
+    password: process.env.BUSCABUS_PASSWORD || 'zp2AcVmaJ2iKZERw'
 };
